2440915fb57f547856cc5ee156dc99077879f626
alrom2525 <edgardoalbertoromero@gmail.com>
2020-01-30 16:26:53 -0500

    Routes

---
+++ procedures/laravel/howto/Laravel back button.php
@@ -0,0 +1,6 @@
+Laravel back button
+https://stackoverflow.com/questions/12333739/laravel-back-button
+<a class="btn btn-dark mt-4" href="{{ URL::previous() }}">Back</a>
+
+<br><br>
+<a  class="btn btn-default" href="{{ url()->previous() }}">Back</a>
